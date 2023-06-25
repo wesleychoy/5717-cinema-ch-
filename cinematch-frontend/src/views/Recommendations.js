2aa32f81dde811eae542f18588b0f11969a4f8ec
wesleychoy <wesleychoy01@gmail.com>
2023-06-26 00:17:43 +0800

    placeholders for movie posters added

--- cinematch-frontend/src/views/Recommendations.js
+++ cinematch-frontend/src/views/Recommendations.js
@@ -1,6 +1,13 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Placeholder from "../assets/movieposters/batman.jpeg"
+import Placeholder1 from "../assets/movieposters/BatmanBegins.jpeg"
+import Placeholder2 from "../assets/movieposters/TheDarkKnightRises.jpeg"
+import Placeholder3 from "../assets/movieposters/DarkKnight.jpeg"
+import Placeholder4 from "../assets/movieposters/InfinityWar.jpeg"
+import Placeholder5 from "../assets/movieposters/BabyDriver.jpeg"
+import Placeholder6 from "../assets/movieposters/TheGreenMile.jpeg"
+import Placeholder7 from "../assets/movieposters/TheBigShort.jpeg"
+import Placeholder8 from "../assets/movieposters/Minions.jpeg"
 
 // Import Swiper styles
 import "swiper/css";
@@ -27,28 +34,28 @@ export default function Recommendations() {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder1} alt="BatmanBegins" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder2} alt="TheDarkKnightRises" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder3} alt="TheDarkKnight" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder4} alt="Avengers" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder5} alt="BabyDriver" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder6} alt="TheGreenMile" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder7} alt="TheBigShort" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder8} alt="Minions" />
           </SwiperSlide>
         </Swiper>
       </div>
@@ -66,28 +73,28 @@ export default function Recommendations() {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder7} alt="TheBigShort" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder2} alt="TheDarkKnightRises" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder8} alt="Minions" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder4} alt="Avengers" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder1} alt="BatmanBegins" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder6} alt="TheGreenMile" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder3} alt="TheDarkKnight" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder5} alt="BabyDriver" />
           </SwiperSlide>
         </Swiper>
       </div>
@@ -105,28 +112,28 @@ export default function Recommendations() {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder6} alt="TheGreenMile" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder2} alt="TheDarkKnightRises" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder5} alt="BabyDriver" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder4} alt="Avengers" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder8} alt="Minions" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder1} alt="BatmanBegins" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder7} alt="TheBigShort" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={Placeholder} alt="Batman" />
+            <img src={Placeholder3} alt="TheDarkKnight" />
           </SwiperSlide>
         </Swiper>
       </div>
