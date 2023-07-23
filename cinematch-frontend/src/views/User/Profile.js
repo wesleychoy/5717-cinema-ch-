a05c26321359691ebe67edd742197f4ca6da20b3
berniceLiew <berniceliew03@gmail.com>
2023-07-24 04:45:10 +0900

    Able to change passwords

--- cinematch-frontend/src/views/User/Profile.js
+++ cinematch-frontend/src/views/User/Profile.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { collection, query, onSnapshot } from '@firebase/firestore';
 import { db, auth } from '../../utils/firebase';
+import { updatePassword } from "firebase/auth";
 import ViewHistory from '../../components/ViewHistory';
 import { Stack } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Alert, Backdrop, TextField, Typography, Button } from '@mui/material';
 
 
 function Profile() {
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirmation, setNewPasswordConfirmation] = useState('');
   const [history, setHistory] = useState([]);
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
   const currentUserUID = auth.currentUser.uid;
   const queryHistory = query(collection(db, "users", `${currentUserUID}`, "history"))
+
   useEffect(() => {
     onSnapshot(queryHistory, (snapshot) => {
       setHistory(snapshot.docs.map(doc => ({
@@ -19,8 +26,53 @@ function Profile() {
     })
   }, []);
 
+  const validatePassword = () => {
+    if (newPassword.length < 6) throw Error('Password is less than 6 characters');
+    if (newPasswordConfirmation != newPassword) throw Error ('Passwords do not match');
+  }
+
+  const handleCloseBackdrop = () => {
+    setSuccess(false);
+  }
+
+  const handleChangePassword = () => {
+    try {
+      validatePassword();
+      updatePassword(auth.currentUser, newPassword).then(() => {
+        console.log('Successfully changed passwords');
+        setSuccess(true);
+      });
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+
   return (
     <Stack container direction={'column'} spacing={2} sx={{ my: 1, p: 3 }}>
+      <Typography variant='h4' color={'black'}>Account</Typography>
+      <Stack container direction={'column'} spacing={2} sx={{ width: 200 }}>
+        <TextField
+          id='new_password'
+          label='New Password'
+          value={newPassword}
+          onChange={(event) => setNewPassword(event.target.value)}/>
+        <TextField
+          id='new_password_confirmation'
+          label='Confirm New Password'
+          value={newPasswordConfirmation}
+          onChange={(event) => setNewPasswordConfirmation(event.target.value)}/>
+        <Button variant='contained' onClick={handleChangePassword}>Change</Button>
+        {errorMessage && (
+          <Alert severity='error'>{errorMessage}</Alert>
+        )}
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={success}
+          onClick={handleCloseBackdrop}
+        >
+          <Alert severity='success'>Password changed!</Alert>
+        </Backdrop>
+      </Stack>
       <Typography variant='h4' color={'black'}>History</Typography>
       <Stack container direction={'column'} spacing={2}>
         <Stack direction={'row'} justifyContent='left' spacing={2}>
