14209ca901ecbcb9bde9c87417a52b14be114ce9
wesleychoy <wesleychoy01@gmail.com>
2023-07-24 11:17:56 +0800

    added placeholder for recos page

--- cinematch-frontend/src/views/Recommendations.js
+++ cinematch-frontend/src/views/Recommendations.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../styles/Recommendations.css";
+import Placeholder1 from "../assets/movieposters/BatmanBegins.jpeg"
+import Placeholder2 from "../assets/movieposters/TheDarkKnightRises.jpeg"
+import Placeholder3 from "../assets/movieposters/DarkKnight.jpeg"
+import Placeholder4 from "../assets/movieposters/InfinityWar.jpeg"
+import Placeholder5 from "../assets/movieposters/BabyDriver.jpeg"
+import Placeholder6 from "../assets/movieposters/TheGreenMile.jpeg"
+import Placeholder7 from "../assets/movieposters/TheBigShort.jpeg"
+import Placeholder8 from "../assets/movieposters/Minions.jpeg"
+
 
 //Import Swiper 
 import "swiper/css";
@@ -60,6 +69,45 @@ const Recommendations = () => {
         )}
       </div>
       <div className='sliders'>
+        <h1>Based on Your Preferences</h1>
+        <div className='LikedCategory'>
+          <Swiper
+            modules={[Pagination, Navigation]}
+            slidesPerView={3}
+            spaceBetween={10}
+            navigation
+            pagination={{
+              clickable: true,
+            }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            <SwiperSlide>
+              <img src={Placeholder7} alt="TheBigShort" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder2} alt="TheDarkKnightRises" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder8} alt="Minions" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder4} alt="Avengers" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder1} alt="BatmanBegins" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder6} alt="TheGreenMile" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder3} alt="TheDarkKnight" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Placeholder5} alt="BabyDriver" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
         <h1>Most Popular Movies</h1>
         <div className='PopularCategory'>
           <Swiper
